ff05003634a3b6b0bb009fbe8668d39b01542f6b
Pengu-90 <148702057+Pengu-90@users.noreply.github.com>
2024-08-27 13:55:54 +0800

    Add files via upload

---
+++ src/main.js
@@ -0,0 +1,5 @@
+const resumeDL_btn = document.getElementById('resumeDL_btn');
+
+ function downloadResume() {
+    location.href = "https://docs.google.com/uc?export=download&id=1agarCmRQOTWdAdEsf6R9oEjiHtMqTf3w";
+};
